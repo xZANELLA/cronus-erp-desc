3451448345dafa947a7baf3d5cb83f5d9341d8a2
Luiz Henrique Cabral <42081493+xZANELLA@users.noreply.github.com>
2022-04-19 16:08:51 -0300

    feat: config express-session

--- index.js
+++ index.js
@@ -2,12 +2,20 @@ const express = require('express')
 const app = express()
 const PORT = process.env.PORT || 8081
 const hostname = "localhost"
+const session = require('express-session')
 
+
+//Sessão
+app.use(session({
+  secret: "cronus-erp",
+  resave: true,
+  saveUninitialized: true
+}))
 
 app.get('/', (req, res) => {
   res.send('Pagina Inicial')
 })
 
-app.listen(PORT, hostname,  () => {
+app.listen(PORT, hostname, () => {
   console.log('Aplicação rodando no ' + hostname + ' na porta ' + PORT)
 })
