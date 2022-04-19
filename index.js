ec2953e80dda923bc6da26eb428453354a04b482
Luiz Henrique Cabral <42081493+xZANELLA@users.noreply.github.com>
2022-04-19 16:09:33 -0300

    feat: config connect-flash

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express()
 const PORT = process.env.PORT || 8081
 const hostname = "localhost"
 const session = require('express-session')
+const flash = require('connect-flash')
 
 
 //Sessão
@@ -11,6 +12,15 @@ app.use(session({
   resave: true,
   saveUninitialized: true
 }))
+
+//Middleware
+app.use((req, res, next) => {
+    res.locals.success_msg = req.flash("success_msg")
+    res.locals.error_msg = req.flash("error_msg")
+    res.locals.error = req.flash("error")
+    res.locals.user = req.user || null
+    next()
+})
 
 app.get('/', (req, res) => {
   res.send('Pagina Inicial')
